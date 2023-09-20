6be64890f2348bfbe9c57adc539e30720f6ade6b
BlacktonRT <blacktonrt@outlook.com>
2023-09-20 16:06:56 -0600

    comentarios

--- hola_luna/main.cpp
+++ hola_luna/main.cpp
@@ -2,38 +2,40 @@
 #include <box2d/box2d.h>
 
 int main() {
-    //===========SUELO========================================
+
     //Creacion del mundo y de la gravedad
     b2Vec2 gravity(0.0f, -1.62f);
     b2World world(gravity);
 
-    //Creacion de las caracteristicas del cuerpo del suelo
+    //===========SUELO========================================
+    //Creacion de las caracteristicas del suelo
     b2BodyDef groundBodyDef;
     groundBodyDef.position.Set(0.0f, -10.0f);
 
-    //Creamos el cuerpo del suelo
+    //Creamos el suelo
     b2Body*groundBody = world.CreateBody(&groundBodyDef);
 
     //Crear su forma del suelo
     b2PolygonShape groundBox;
     groundBox.SetAsBox(50,1);
 
-    groundBody->CreateFixture(&groundBox, 0.0f);
+    groundBody->CreateFixture(&groundBox, 0.0f);//propiedades fisicas del suelo
 
     //===========OBJETO=======================================
+    //Creacion de las caracteristicas de la caja
     b2BodyDef bodyDef;
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(0.0f, 20.0);
+    bodyDef.type = b2_dynamicBody; //definicion de un objeto dinamico porque se va a mover
+    bodyDef.position.Set(0.0f, 20.0); //posicion del objeto
 
-    b2Body* body = world.CreateBody(&bodyDef);
+    b2Body* body = world.CreateBody(&bodyDef); //instanciar el objeto
 
-    b2PolygonShape dynamicBox;
-    dynamicBox.SetAsBox(1.0f, 1.0f);
+    b2PolygonShape dynamicBox; //darle propiedad de poligono
+    dynamicBox.SetAsBox(1.0f, 1.0f); //darle forma de caja al objeto
 
-    b2FixtureDef fixtureDef;
-    fixtureDef.shape = &dynamicBox;
-    fixtureDef.density = 1.0f;
-    fixtureDef.friction = 0.03f;
+    b2FixtureDef fixtureDef; //definicion del fixture, el cual ya cuenta con propiedades
+    fixtureDef.shape = &dynamicBox; //forma de caja
+    fixtureDef.density = 1.0f; //densidad
+    fixtureDef.friction = 0.03f; //friccion menor porque está en la luna
 
     body ->CreateFixture(&fixtureDef);
 
@@ -42,11 +44,12 @@ int main() {
     int32 velocityIterations = 6;
     int32 positionIteration = 2;
 
+    //Ciclo for para tomar las coordenadas del objeto cayendo a 60 iteraciones por segundo
     for (int32 i = 0; i < 60; ++i) {
-        world.Step(timeStep, velocityIterations, positionIteration);
-        b2Vec2 position = body->GetPosition();
-        float angle = body->GetAngle();
-        printf("%4.2f %4.2f %4.2f\n", position.x,position.y,angle);
+        world.Step(timeStep, velocityIterations, positionIteration); //mueve las iteraciones del paso del tiempo en el mundo
+        b2Vec2 position = body->GetPosition(); //obtiene la posicion en x y y del objeto, x no cambia
+        float angle = body->GetAngle(); //obtiene el angulo del objeto cayendo
+        printf("%4.2f %4.2f %4.2f\n", position.x,position.y,angle);//imprime coordenadas de la caja cayendo
     }
 
 }
